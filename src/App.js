e5912bbb10fbe95ad45aaf622d5deb448156717f
lauher <lauraherbert90@hotmail.com>
2021-09-19 13:13:09 +0200

    Added search engine component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo-shecodes.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo img-fluid" alt="logo SheCodes" />
+      <div className="container">
+        <header className="App-header"></header>
+        <img src={logo} className="App-logo img-fluid" alt="logo SheCodes" />
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small> Coded by Laura Herbert </small>
+        </footer>
+      </div>
     </div>
   );
 }
